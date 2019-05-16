268f3cd469f3c43ee0f7ceb2efb6d5706a44116b
java <957904176@qq.com>
2019-05-16 18:39:25 +0800

    日常更新

---
+++ 界面2/js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    console.log('yes, jquery is in document')
+    $('.navbar-brand .navbar-text').fadeIn('slow');
+    $('.navbar-brand .navbar-text').slideDown();
+})
